476685bea0f5bb61eb2ac3ef9aec163a86013774
SARTOR Fabien <fabien.sartor@secheron.com>
2018-05-24 08:11:14 +0200

    [DEV] Add one shared circular buffer for reactor

--- rs-jsynexample/src/main/java/fs/fasar/test/javasound/TestMicReactor2.java
+++ rs-jsynexample/src/main/java/fs/fasar/test/javasound/TestMicReactor2.java
@@ -9,6 +9,9 @@ import java.time.Instant;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestMicReactor2 {
+
+    private static final int NB_BUFFER = 100;
+
     public static void main(String[] args) throws InterruptedException {
         AudioFormat format = new AudioFormat(44100, 16, 2, true, true);
         format = MyFormat.get();
@@ -26,35 +29,34 @@ public class TestMicReactor2 {
             sourceLine.start();
 
             int bufferSizeOld = targetLine.getBufferSize() / 5;
-            int bufferSize = 40;
+            int bufferSize = 4000;
+            byte[] targetData = new byte[bufferSize * NB_BUFFER];
 
-            AtomicInteger micLoop = new AtomicInteger(0);
-            Flux<Wrapper> flux = Flux.<Wrapper>generate((sink) -> {
-                Instant start = Instant.now();
-                byte[] targetData2 = new byte[bufferSize];
-                int read = targetLine.read(targetData2, 0, bufferSize);
-                if(read != bufferSize) {
-                    System.out.println("Read only " + read);
-                    return;
-                }
-                sink.next(new Wrapper(targetData2, start));
-                int i1 = micLoop.incrementAndGet();
-                if ((i1 % 11) == 1) {
-                    //System.out.println("Read MIC on thread :" + Thread.currentThread().getName());
-                }
-            }).subscribeOn(Schedulers.elastic());
+            Flux<Wrapper> flux = Flux.generate(
+                    () -> NB_BUFFER-1,
+                    (acc, sink) -> {
+                        int currentIndex = acc + 1;
+                        if(currentIndex % (NB_BUFFER) == 0) {
+                            currentIndex = 0;
+                        }
+                        Instant start = Instant.now();
+                        int read = targetLine.read(targetData, currentIndex * bufferSize, bufferSize);
+                        sink.next(new Wrapper(currentIndex, read, start));
+                        return currentIndex;
+                    });
+            flux = flux.subscribeOn(Schedulers.elastic());
 
             AtomicInteger readLoop = new AtomicInteger(0);
             flux
                     .publishOn(Schedulers.parallel())
                     .subscribe(wrapper -> {
-                        sourceLine.write(wrapper.buffer, 0, bufferSize);
+                        sourceLine.write(targetData, wrapper.buffPos * bufferSize, wrapper.read);
                         int i1 = readLoop.incrementAndGet();
                         if ((i1 % 11) == 1) {
                             //System.out.println("Read SND on thread :" + Thread.currentThread().getName());
                             Instant stop = Instant.now();
                             Duration between = Duration.between(wrapper.ts, stop);
-                            System.out.println("Read/Write sound takes : " + between.toString() + " " + between.getSeconds() + "s " + between.getNano() +"nano");
+                            System.out.println("Read/Write sound takes : " + between.toString() + " " + between.getSeconds() + "s " + between.getNano() + "nano");
                         }
                     });
 
@@ -66,11 +68,13 @@ public class TestMicReactor2 {
     }
 
     public static class Wrapper {
-        public final byte[] buffer;
+        public final int buffPos;
+        public final int read;
         public final Instant ts;
 
-        public Wrapper(byte[] buffer, Instant ts) {
-            this.buffer = buffer;
+        public Wrapper(int buffPos, int read, Instant ts) {
+            this.buffPos = buffPos;
+            this.read = read;
             this.ts = ts;
         }
     }
